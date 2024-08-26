1cfee53c93af838f7c992b250648c5b5bdd145cc
gyenoch <www.gyenoch@gmail.com>
2024-08-26 03:32:44 +0000

    main.tf updated

--- EC2-terraform/Main.tf
+++ EC2-terraform/Main.tf
@@ -1,10 +1,10 @@
 resource "aws_security_group" "Jenkins-sg" {
-  name        = "Jenkins-Security Group"
-  description = "Open 22,443,80,8080,9000,9100,9090,3000"
+  name        = "Jenkins-Security-Group"
+  description = "Open 22,443,465,80,8080,9000,9100,9090,3000"
 
   # Define a single ingress rule to allow traffic on all specified ports
   ingress = [
-    for port in [22, 80, 443, 8080, 9000,9100,9090,3000] : {
+    for port in [22, 80, 443,465,8080,9000,9100,9090,3000] : {
       description      = "TLS from VPC"
       from_port        = port
       to_port          = port
@@ -29,26 +29,77 @@ resource "aws_security_group" "Jenkins-sg" {
   }
 }
 
+# DATA For AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# Create the IAM Role and Attach the Administrator Policy
+resource "aws_iam_role" "reddit_clone_role" {
+  name = "reddit_clone-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "reddit_clone_role_attach" {
+  role       = aws_iam_role.reddit_clone_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# Create an Instance Profile and Attach the Role
+resource "aws_iam_instance_profile" "reddit_clone_instance_profile" {
+  name = "reddit_clone-instance-profile"
+  role = aws_iam_role.reddit_clone_role.name
+}
+
+
+# JENKINS SONARQUBE EC2 INSTANCE
 resource "aws_instance" "web" {
-  ami                    = "ami-0522ab6e1ddcc7055"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.large"
-  key_name               = "my key"
+  key_name               = "jenkins"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./EC2.sh", {})
 
+  iam_instance_profile = aws_iam_instance_profile.reddit_clone_instance_profile.name
+
   tags = {
-    Name = "amazon clone"
+    Name = "reddit-clone"
   }
   root_block_device {
     volume_size = 30
   }
 }
 resource "aws_instance" "web2" {
-  ami                    = "ami-0c7217cdde317cfec"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.medium"
-  key_name               = "my key"
+  key_name               = "jenkins"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+
+  iam_instance_profile = aws_iam_instance_profile.reddit_clone_instance_profile.name
   tags = {
     Name = "Monitering via grafana"
   }
